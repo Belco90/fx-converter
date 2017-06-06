e5186b4e16256a8bb291b1e15c337bb0e03981ad
Mario Beltrán Alarcón <belco90@gmail.com>
2017-06-07 00:50:36 +0200

    Basic call to API

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,11 +4,23 @@ import PropTypes from 'prop-types';
 
 class MainSection extends React.Component {
   render() {
-    const rates = Object.entries(this.props.latestRates).map((rate, idx) => {return this.renderRate(rate, idx)});
+    if (this.props.latestRates) {
+      const rates = Object.entries(this.props.latestRates).map((rate, idx) => {return this.renderRate(rate, idx)});
+
+      return (
+        <div>
+          <h1>Latest FX Rates ({this.props.date})</h1>
+          <h3>Base currency: {this.props.base}</h3>
+          <ul>{rates}</ul>
+        </div>
+      )
+    }
+
+    // default when something went wrong calling API
     return (
       <div>
         <h1>Latest FX Rates</h1>
-        <ul>{rates}</ul>
+        <p>Sorry, something went wrong</p>
       </div>
     )
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import MainSection from '../components/MainSection';
+import request from 'superagent';
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      rates: {
-        'GBP': 1.23,
-        'EUR': 3.21
-      }
+      date: null,
+      base: null,
+      rates: null
     };
+  }
+
+  componentDidMount() {
+    request.get('http://api.fixer.io/latest')
+      .end((err, res) => {
+        if (err) {
+          this.setState({rates: null});
+        } else {
+          this.setState(res.body);
+        }
+      });
   }
 
   render() {
     return (
     <div>
       <MainSection
+        base={this.state.base}
+        date={this.state.date}
         latestRates={this.state.rates}
       />
     </div>
