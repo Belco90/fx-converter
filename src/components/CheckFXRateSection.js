f3a606491395e046086ea49bd49872fda13e9505
Mario Beltrán Alarcón <belco90@gmail.com>
2017-06-15 00:18:24 +0200

    Use state to store form values instead of taking them from DOM

--- src/components/CheckFXRateSection.js
+++ src/components/CheckFXRateSection.js
@@ -1,58 +1,32 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import CurrencySelect from './CurrencySelect';
-import RateResult from './RateResult';
+import PropTypes from 'prop-types'
+import React from 'react'
+import RateResult from './RateResult'
+import FXRateForm from './FXRateForm'
 
 class CheckFXRateSection extends React.Component {
-  render() {
+  render () {
     return (
       <div className="currency-pair-fx-rate-section">
         <h1>Check FX Rate</h1>
 
         <div className="row">
           <div className="col-md-6">
-            <form action="." method="GET" onSubmit={e => {this.onSubmitForm(e, this.props.fetchFXRateCb)}}>
-
-              <div className="form-group">
-                <label htmlFor="base-select">Sell Currency</label>
-                <CurrencySelect name="base" defaultValue="EUR" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="symbols-select">Buy Currency</label>
-                <CurrencySelect name="symbols" defaultValue="GBP" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="date-input">Date</label>
-                <input type="date" id="date-input" min="1999-01-02" className="form-control" required/>
-              </div>
-
-              <button type="submit" className="btn btn-primary">Check</button>
-            </form>
+            <FXRateForm fetchFXRateCb={this.props.fetchFXRateCb}/>
           </div>
 
           <div className="col-md-6">
-            <RateResult rate={this.props.checkedRate} />
+            <RateResult rate={this.props.checkedRate}/>
           </div>
         </div>
       </div>
-    );
+    )
   }
 
-  onSubmitForm(e, cb) {
-    e.preventDefault();
-    let date = document.getElementById('date-input').value;
-    let sellCurrency = document.getElementById('base-select').value;
-    let buyCurrency = document.getElementById('symbols-select').value;
-
-    cb(date, sellCurrency, buyCurrency);
-  }
 }
 
 CheckFXRateSection.PropTypes = {
   fetchFXRateCb: PropTypes.func.isRequired,
   checkedRate: PropTypes.object.isRequired,
-};
+}
 
-export default CheckFXRateSection;
+export default CheckFXRateSection
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import App from './containers/App';
-import configureStore from './store/configureStore';
+import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import configureStore from './store/configureStore'
 
 const store = configureStore({
-  latestRates: [
-    {
-      rates: {},
-      base: null,
-      date: null,
-      err: true,
-    }
-  ]
-});
+  latestRates: [{
+    rates: {},
+    base: null,
+    date: null,
+    err: true,
+  }],
+  checkedRates: [{
+    date: null,
+    sellCurrency: null,
+    buyCurrency: null,
+    amount: null,
+    err: true,
+  }]
+
+})
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
